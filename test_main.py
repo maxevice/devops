f6ea7d52ea6a1886a2636956361ef0a55be3bdbb
Student 17 <student17@university.edu>
2026-05-24 00:14:17 +0000

    ci: налаштування лінтерів, тестів з покриттям понад 40% та збірки образу в GHCR

---
+++ test_main.py
@@ -0,0 +1,12 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+def test_health_alive():
+    assert response.status_code == 200
+
+def test_read_items_json():
+    response = client.get("/items", headers={"Accept": "application/json"})
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)
